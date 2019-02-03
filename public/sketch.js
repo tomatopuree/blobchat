0d3842b57142f6dc95102243bd6d1040870196c0
tomatopuree <addoagrucu@gmail.com>
2019-02-02 22:45:52 -0500

    Comments

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,34 @@
-function Server(blobsdict, chatslist, serverpermanents) {
-  this.blobsdict = blobsdict;
-  this.chatslist = chatslist;
-  this.serverpermanents = serverpermanents;
+function Blob(name, id, x, y, r) {
+	this.lastchat = "";
+	this.lastchattime;
+	this.name = name;
+	this.id = id;
+	this.pos = createVector(x, y);
+	this.r = r;
+	this.vel = createVector(0, 0);
+
+	/// update position and smooth velocity
+	this.update_position = function() {
+		var newvel = createVector(mouseX - width / 2, mouseY - height / 2).div(50);
+		// this is the speed limit
+		newvel.limit(1);
+		this.vel.lerp(newvel, 0.2);
+		this.pos.add(this.vel);
+	}
+
+	this.show_and_constrain_this_blob = function() {
+		blob.pos.x = constrain(blob.pos.x, -width / 4, width / 4);
+		blob.pos.y = constrain(blob.pos.y, -height / 4, height / 4);
+		fill(255);
+		ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
+	}
 }
 
-
-
+function Server(blobsdict, chatslist, serverpermanents) {
+	this.blobsdict = blobsdict;
+	this.chatslist = chatslist;
+	this.serverpermanents = serverpermanents;
+}
 
 currentserver = new Server(blobsdict = {}, chatslist = [], serverpermanents = []);
 
@@ -20,196 +43,221 @@ var serverpermanents = [];
 var zoom = 1;
 var input, button, greeting;
 
-var resolutionW = 1440;
-var resolutionH = 900;
+var resolutionW = 1400;
+var resolutionH = 800;
+
 
 function setup() {
-  
-  createCanvas(resolutionW, resolutionH);
-  
-  socket = io.connect('https://damp-citadel-76206.herokuapp.com/');
-  // socket = io.connect('http://localhost:3000');
+	
+	createCanvas(resolutionW, resolutionH);
+	
+	// socket = io.connect('https://damp-citadel-76206.herokuapp.com/');
+	socket = io.connect('http://localhost:3000');
 
-  // CHAT INPUT
-  input = createInput();
-  input.position((resolutionW*(0.7/20)), (resolutionH*(19.1/20)));
+	create_chat_pane();
 
-  button = createButton('Enter');
-  button.position(input.x + input.width, (resolutionH*(19.1/20)));
-  button.mousePressed(chatin);
+	create_name_pane();
 
-  // NAME INPUT
-  input2 = createInput();
-  input2.position((resolutionW*(0.7/20)), (resolutionH*(0.7/20)));
+	blob = new Blob(socket.id, socket.id, 0, 0, random(18,24));
+	var data = {
+		x: blob.pos.x,
+		y: blob.pos.y,
+		r: blob.r
+	};
 
-  button2 = createButton('Submit Name');
-  button2.position(input2.x + input.width, (resolutionH*(0.7/20)));
-  button2.mousePressed(namein);
+	socket.emit('start', data);
 
-  textAlign(CENTER);
-  textSize(50);
-
-  // MAKE BLOB
-  blob = new Blob(socket.id, socket.id, 0, 0, random(18,24));
-  var data = {
-    x: blob.pos.x,
-    y: blob.pos.y,
-    r: blob.r
-  };
-
-  socket.emit('start', data);
-
-  socket.on('heartbeat',
-    function(data) {
-      chatslist = data[0].chatslist;
-      blobsdict = data[0].blobsdict;
-      serverpermanents = data[0].serverpermanents;
-      currentservername = data[1];
-    }
-  );
+	socket.on('heartbeat',
+		function(data) {
+			chatslist = data[0].chatslist;
+			blobsdict = data[0].blobsdict;
+			serverpermanents = data[0].serverpermanents;
+			currentservername = data[1];
+		}
+	);
 }
-
-
 
 function draw() {
-  background(200);
+	
+	background(200);
 
-  // DRAW CHAT WINDOW
-  for (var i = 0; i < chatslist.length; i++) {
-    textAlign(LEFT);
-    textSize(18);
-    var time = +new Date;
+	draw_chat_pane();
+	
+	draw_metadata();
 
-    fill(0, 0, 0, 255);
+	// Anything before below chunk of code will be anchored to "camera"
+	// Anything after below chunk of code will not be anchored to "camera"
+	// "camera" follows blob, scale w/ size
 
-    var username = chatslist[i][2];
-    text(username + ": ", ~~(resolutionW*(0.6/20)), ~~((resolutionH*(16/20))+i*30));
-    
-    fill(255, 255, 255, 255);;
+	// put blob in middle
+	translate(width / 2, height / 2);
+	// how much zoom and how fast zoom
+	zoom = lerp(zoom, 64 / blob.r, 0.1);
+	scale(zoom);
+	// camere follow
+	translate(-blob.pos.x, -blob.pos.y);
 
-    var userschat = chatslist[i][0];
-    text(userschat, ~~(resolutionW*(0.6/20)) + textWidth(username)+8, ~~((resolutionH*(16/20))+i*30));
-    }
-  
+	draw_other_blobs();
 
-  fill('red');
-  textAlign(CENTER);
-  textSize(10);
-  text(blob.pos.x + " : " + blob.pos.y, ~~(resolutionW*(18/20)), ~~(resolutionH*(19/20)));
+	draw_server_permanents();
 
-  textAlign(CENTER);
-  textSize(10);
-  text("People in room: " + Object.keys(blobsdict).length, ~~(resolutionW*(18/20)), ~~(resolutionH*(18.5/20)));
+	blob.show_and_constrain_this_blob();
+	if (mouseIsPressed) {
+		blob.update_position();
+	}
 
-  console.log(socket.rooms);
-  text("In room " + currentservername, ~~(resolutionW*(18/20)), ~~(resolutionH*(18/20)));
-
-
-  // Anything before this chunk of code will be anchored to "camera"
-  // Anything after this chunk of code will not be anchored to "camera"
-  // "camera" follows blob, scale w/ size
-  translate(width / 2, height / 2);
-  var newzoom = 64 / blob.r;
-  zoom = lerp(zoom, newzoom, 0.1);
-  scale(zoom);
-  translate(-blob.pos.x, -blob.pos.y);
-
-
-  // Draw names and chats
-  for (var keyy in blobsdict) {
-    var id = socket.id;
-
-    fill(0, 0, 255);
-    ellipse(blobsdict[keyy].x, blobsdict[keyy].y, blobsdict[keyy].r * 2, blobsdict[keyy].r * 2);
-
-    textAlign(CENTER);
-    textSize(4);
-    fill(255);
-    text(blobsdict[keyy].lastchat, blobsdict[keyy].x, blobsdict[keyy].y + blobsdict[keyy].r + 4);
-    fill(0);
-    text(blobsdict[keyy].name, blobsdict[keyy].x, blobsdict[keyy].y - (blobsdict[keyy].r+2));
-
-  }
-
-  //  create world using server permanents
-  for (var k = 0; k < serverpermanents.length; k++) {
-
-    // portal & portaltext
-    fill(255);
-    ellipse(serverpermanents[k].x, serverpermanents[k].y, serverpermanents[k].r, serverpermanents[k].r);
-    fill('blue');
-    textAlign(CENTER);
-    textSize(6);
-    text(serverpermanents[k].name, serverpermanents[k].x-1, -5);
-  }
-
-
-  blob.show();
-  if (mouseIsPressed) {
-    blob.updatepos();
-  }
-  blob.constrain();
-
-  var data = {
-    x: blob.pos.x,
-    y: blob.pos.y,
-    r: blob.r
-  };
-  socket.emit('update', data);
-
-
-
+	var data = {
+		x: blob.pos.x,
+		y: blob.pos.y,
+		r: blob.r
+	};
+	socket.emit('update', data);
 }
 
 
 
+////////////////////////////////////////////////////
+////////////////// SETUP FUNCTIONS //////////////////
+////////////////////////////////////////////////////
 
-//////////// BUTTONS AND KEY PRESSES
+// doesn't scroll, regular form
+function create_chat_pane() {
+	input = createInput();
+	input.position((resolutionW*(0.7/20)), (resolutionH*(19.1/20)));
 
-/// Handles all key presses
+	button = createButton('Enter');
+	button.position(input.x + input.width, (resolutionH*(19.1/20)));
+	button.mousePressed(chatin);
+}
+
+// cycling panes
+function create_name_pane() {
+	input2 = createInput();
+	input2.position((resolutionW*(0.7/20)), (resolutionH*(0.7/20)));
+
+	button2 = createButton('Submit Name');
+	button2.position(input2.x + input2.width, (resolutionH*(0.7/20)));
+	button2.mousePressed(namein);
+
+	textAlign(CENTER);
+	textSize(50);
+}
+
+/// handles all key presses
 function keyPressed() {
-  if (keyCode == ENTER) {
-    chatin();
-  }
+	if (keyCode == ENTER) {
+		chatin();
+	}
 }
 
-///  name input, sends name to server
-function namein() {
-  var name = input2.value();
-
-  var data = {
-    id: blob.id,
-    name: name
-  };
-
-  socket.emit('name', data);
-  button2.hide();
-  input2.hide();
-
-  button3 = createButton('Rename Character');
-  button3.position((resolutionW*(0.7/20)), (resolutionH*(0.7/20)));
-  button3.mousePressed(rename);
-}
-
-/// helper
-function rename() {
-  button3.hide();
-  button2.show();
-  input2.show();
-}
-
-/// chat input, sends chat to server
+/// sends chat to server
 function chatin() {
-  var chat = input.value();
+	var chat = input.value();
 
-  var time = +new Date;
-  var data = {
-    name: blobsdict[socket.id].name,
-    chat: chat,
-    time: time
-  };
-  
-  socket.emit('chat', data);
+	var time = +new Date;
+	var data = {
+		name: blobsdict[socket.id].name,
+		chat: chat,
+		time: time
+	};
+	
+	socket.emit('chat', data);
 
-  input.value('');
+	input.value('');
+}
+
+///  sends name to server
+function namein() {
+	var name = input2.value();
+
+	var data = {
+		id: blob.id,
+		name: name
+	};
+
+	socket.emit('name', data);
+	button2.hide();
+	input2.hide();
+
+	button3 = createButton('Rename Character');
+	button3.position((resolutionW*(0.7/20)), (resolutionH*(0.7/20)));
+	button3.mousePressed(rename);
+}
+
+// from rename state to original state 
+function rename() {
+	button3.hide();
+	button2.show();
+	input2.show();
+}
+
+
+
+////////////////////////////////////////////////////
+////////////////// DRAW FUNCTIONS //////////////////
+////////////////////////////////////////////////////
+
+// update with chats
+function draw_chat_pane() {
+	for (var i = 0; i < chatslist.length; i++) {
+		textAlign(LEFT);
+		textSize(18);
+
+		fill(0, 0, 0, 255);
+
+		var username = chatslist[i][2];
+		text(username + ": ", ~~(resolutionW*(0.6/20)), ~~((resolutionH*(16/20))+i*30));
+		
+		fill(255, 255, 255, 255);;
+
+		var userschat = chatslist[i][0];
+		text(userschat, ~~(resolutionW*(0.6/20)) + textWidth(username)+8, ~~((resolutionH*(16/20))+i*30));
+	}
+}
+
+// update with metadatas
+function draw_metadata() {
+	fill('red');
+	textAlign(CENTER);
+	textSize(10);
+
+	// room name
+	text("In room " + currentservername, ~~(resolutionW*(18/20)), ~~(resolutionH*(18/20)));
+
+	// people in room
+	text("People in room: " + Object.keys(blobsdict).length, ~~(resolutionW*(18/20)), ~~(resolutionH*(18.5/20)));
+
+	// coordinates
+	text(blob.pos.x + " : " + blob.pos.y, ~~(resolutionW*(18/20)), ~~(resolutionH*(19/20)));
+}
+
+// update with other blob info
+function draw_other_blobs() {
+	for (var keyy in blobsdict) {
+		fill(0, 0, 255);
+		ellipse(blobsdict[keyy].x, blobsdict[keyy].y, blobsdict[keyy].r * 2, blobsdict[keyy].r * 2);
+
+		textAlign(CENTER);
+		textSize(4);
+		fill(255);
+		text(blobsdict[keyy].lastchat, blobsdict[keyy].x, blobsdict[keyy].y + blobsdict[keyy].r + 4);
+		
+		fill(0);
+		text(blobsdict[keyy].name, blobsdict[keyy].x, blobsdict[keyy].y - (blobsdict[keyy].r+2));
+	}
+}
+
+// update with non-blob server permanents
+function draw_server_permanents() {
+	//  create world using server permanents
+	for (var k = 0; k < serverpermanents.length; k++) {
+		// portal & portaltext
+		fill(255);
+		ellipse(serverpermanents[k].x, serverpermanents[k].y, serverpermanents[k].r, serverpermanents[k].r);
+		
+		fill('blue');
+		textAlign(CENTER);
+		textSize(6);
+		text(serverpermanents[k].name, serverpermanents[k].x-1, -5);
+	}
 }
--- server.js
+++ server.js
@@ -26,35 +26,29 @@ function Blob(name, id, x, y, r) {
 	this.lastchattime;
 }
 
-
-
 function Server(blobsdict, chatslist, serverpermanents) {
 	this.blobsdict = blobsdict;
 	this.chatslist = chatslist;
 	this.serverpermanents = serverpermanents;
 }
 
-
-
 var serversdict = {};
 var usertoservermap = {};
 
-
 /// Register a callback function upon each individual connection
 io.sockets.on('connection',
-
 	function(socket) {
-
-		// ROOM 
+		// there is no way to access socket's room inherently in socket.io. 
+		// that's why socket.join and socket.leave aren't enough, we need
+		// usertoservermap to keep track of a map of user to server
+		// and dynamically update it
 		socket.join('lobby');
-
 
 		// keep track of which server this guy is connected to
 		usertoservermap[socket.id] = 'lobby';
 
-		// no one's here so make THE LOBBY
+		// no one's here so make the lobby
 		if (Object.keys(serversdict).length == 0 && !('lobby' in serversdict)) {
-
 			// lobby server permanents
 			var serverpermanents = [];
 			portal1 = new Blob("Portal to Game", "togame", 150, 0, 5);
@@ -68,15 +62,11 @@ io.sockets.on('connection',
 		}
 
 
-		/// Continuously sends [blobsdict, chatslist] 
 		setInterval(heartbeat, 33);
 		function heartbeat() {
-			// io.sockets.emit('heartbeat', serversdict[usertoservermap[socket.id]]);
-			// ROOM
-			// socket.rooms is a dictionary nonsensical key/value pairs 
-			io.to(Object.keys(socket.rooms)[1]).emit('heartbeat', [serversdict[usertoservermap[socket.id]], usertoservermap[socket.id]]);
+			// route correct room information to the correct client depending on their server 
+			io.to(usertoservermap[socket.id]).emit('heartbeat', [serversdict[usertoservermap[socket.id]], usertoservermap[socket.id]]);
 		}
-
 
 		console.log("New client joined with socket ID " + socket.id);
 	
@@ -91,14 +81,14 @@ io.sockets.on('connection',
 		/// Upon chat input of user
 		socket.on('chat', 
 			function(data) {
-				// 
 				console.log("Client " + serversdict[usertoservermap[socket.id]].blobsdict[socket.id].name + " says: " + data.chat);
+				// chat is saved in blob (for on blob print) and chats list
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].lastchat = data.chat;
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].lastchattime = data.time;
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].name = data.name;  
 				serversdict[usertoservermap[socket.id]].chatslist.push([data.chat, data.time, data.name]);
 
-				// make room for new chats
+				// pseudo-scrolling of incoming chats
 				if (serversdict[usertoservermap[socket.id]].chatslist.length >= 6) {
 					serversdict[usertoservermap[socket.id]].chatslist.shift();
 				}
@@ -109,24 +99,18 @@ io.sockets.on('connection',
 		socket.on('start',
 			function(data) {
 				var blob = new Blob(socket.id, socket.id, data.x, data.y, data.r);
-
 				// spawn in lobby
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id] = blob; 
-
-				// blobsdict[socket.id] = blob;
-				// console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
-			
 			}
 		);
 
 		/// Update event is continuously received
+		/// this is where checks that must be made continuously must be made
 		socket.on('update',
 		 	function(data) {
-
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].x = data.x;	
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].y = data.y;
 				serversdict[usertoservermap[socket.id]].blobsdict[socket.id].r = data.r;
-
 
 				// (in s) determines how long last chat stay visible under blob
 				var chatDisappear1 = 5;
@@ -142,14 +126,14 @@ io.sockets.on('connection',
 					if (time < value[1]+(chatDisappear2*1000)) {
 						return value;
 					}
-				});
-
+			});
 
 				// check collision with currently displayed server permanents
 				tblob = serversdict[usertoservermap[socket.id]].blobsdict[socket.id];
 				for (var k = 0; k < serversdict[usertoservermap[socket.id]].serverpermanents.length; k++) {
 					portal1 = serversdict[usertoservermap[socket.id]].serverpermanents[k];
 					if ((portal1.r + tblob.r) > (Math.sqrt(Math.pow(Math.abs(tblob.x - portal1.x),2)+Math.pow(Math.abs(tblob.y - portal1.y),2)))) {
+						// if we are going to game server and game server wasnt initialized
 						if (portal1.id == "togame" && !('game' in serversdict)) {
 							var chatslist = [];
 							var blobsdict = {};
@@ -157,7 +141,6 @@ io.sockets.on('connection',
 							// game server world permanents
 							portal3 = new Blob("Portal back to Lobby", "tolobby", 0, 40, 5);
 							serverpermanents.push(portal3);
-							
 							// put new server in serversdict
 							gameserver = new Server(blobsdict, chatslist, serverpermanents);
 							serversdict['game'] = gameserver;
@@ -165,7 +148,7 @@ io.sockets.on('connection',
 
 						if (portal1.id == "togame") {
 							socket.leave('lobby');
-							socket.join('game');	
+							socket.join('game');
 							usertoservermap[socket.id] = 'game';
 							// put blob in new server
 							serversdict[usertoservermap[socket.id]].blobsdict[socket.id] = serversdict['lobby'].blobsdict[socket.id];
@@ -175,25 +158,16 @@ io.sockets.on('connection',
 
 						if (portal1.id == "tolobby") {
 							socket.leave('game');
-							socket.join('lobby');	
+							socket.join('lobby');
 							usertoservermap[socket.id] = 'lobby';
 							// put blob in new server
 							serversdict[usertoservermap[socket.id]].blobsdict[socket.id] = serversdict['game'].blobsdict[socket.id];
 							// take blob off old server
 							delete serversdict['game'].blobsdict[socket.id];
 						}
-
+					}  
 				}
-				  
-
-
-				}
-
-			
-
-
-		 	}
-		);
+		 });
  
 
 		/// Upon disconnect of user
@@ -203,29 +177,6 @@ io.sockets.on('connection',
 		 	// delete blobsdict[socket.id];
 		 	delete serversdict[usertoservermap[socket.id]].blobsdict[socket.id];
 		});
-
-
-
-		// socket.on('serverchange', function(data) {
-		// 	// console.log("Len of dict is " + Object.keys(blobsdict).length);
-
-		// 	// make game server if there isnt one
-
-
-		 		
-		//  	// this user is in game server now
-		//  	// console.log(Object.keys(serversdict));
-
-
-
-
-		// 	// console.log("Client " + serversdict[usertoservermap[socket.id]].blobsdict[socket.id].name + " changed server to " + usertoservermap[socket.id]);
-
-
-
-
-
-		// });
   }
 );
 
